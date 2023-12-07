5bc2c3754064ecfdf601bf62410094ed0dc42d9f
Andrew <andrew@andrewsmith.org>
2023-12-07 18:20:30 -0500

    Solve day 1 puzzle 1

--- day01/puzzle01/solution.js
+++ day01/puzzle01/solution.js
@@ -1,3 +1,5 @@
+const fs = require('node:fs');
+
 const getCalibrationValue = (string) => {
   const numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
@@ -13,9 +15,20 @@ const getCalibrationValue = (string) => {
   }
   const secondNum = string[j];
 
-  return parseInt(firstNum) + parseInt(secondNum);
+  return parseInt(firstNum + secondNum);
 };
 
-console.log(getCalibrationValue('heightseven4two5'));
-console.log(getCalibrationValue('npskfdstpk2knsm'));
-console.log(getCalibrationValue('djnrmpxjbsbpgzvtjkhq6pkkfshx'));
+const input = fs.readFile('./input.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  data = data.split(/\r\n|\r|\n/);
+  let sum = 0;
+  data.forEach((line) => {
+    sum += getCalibrationValue(line);
+    console.log(sum, line);
+  });
+});
+
+// above readFile "works," sort of -- it ends up spitting out the correct solution in the console, but I'm totally unfamiliar with fs methods and I've done something wrong with the callbacks here such that the program doesn't terminate.  futzed with it for a while to no avail so I'll move on for now, but would love to come back and wrap my head around this in the future.
